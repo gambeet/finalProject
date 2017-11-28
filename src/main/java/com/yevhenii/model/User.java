7443b4be7c69f2899a910ac1e67b70b75cbed9a5
gambeet <koolevgesha@gmail.com>
2017-11-28 15:34:47 +0200

    Initial commit

---
+++ src/main/java/com/yevhenii/model/User.java
@@ -0,0 +1,76 @@
+package com.yevhenii.model;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+/**
+ * Created by Yevhenii on 28.11.2017.
+ */
+@Entity
+public class User {
+    private int id;
+    private String login;
+    private String password;
+    private Collection<UserToRole> userToRolesById;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (login != null ? !login.equals(user.login) : user.login != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @OneToMany(mappedBy = "userByUserId")
+    public Collection<UserToRole> getUserToRolesById() {
+        return userToRolesById;
+    }
+
+    public void setUserToRolesById(Collection<UserToRole> userToRolesById) {
+        this.userToRolesById = userToRolesById;
+    }
+}
---
+++ src/main/java/com/yevhenii/model/UserToRole.java
@@ -0,0 +1,60 @@
+package com.yevhenii.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Yevhenii on 28.11.2017.
+ */
+@Entity
+public class UserToRole {
+    private int id;
+    private User userByUserId;
+    private Role roleByRoleId;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserToRole that = (UserToRole) o;
+
+        if (id != that.id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "userId", referencedColumnName = "id", nullable = false)
+    public User getUserByUserId() {
+        return userByUserId;
+    }
+
+    public void setUserByUserId(User userByUserId) {
+        this.userByUserId = userByUserId;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "roleId", referencedColumnName = "id")
+    public Role getRoleByRoleId() {
+        return roleByRoleId;
+    }
+
+    public void setRoleByRoleId(Role roleByRoleId) {
+        this.roleByRoleId = roleByRoleId;
+    }
+}
